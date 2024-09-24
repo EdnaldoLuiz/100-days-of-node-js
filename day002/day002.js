b778545a9e4fc2862da5fcdef6cfbbb9e540b941
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2024-09-23 21:56:11 -0300

    feat: day #002 complete, add js file

---
+++ day002/day002.js
@@ -0,0 +1,4 @@
+console.log("Diretório do script em execução (__dirname):", __dirname);
+console.log("Caminho completo do script em execução (__filename):", __filename);
+console.log("Argumentos passados na linha de comando (process.argv):", process.argv);
+console.log("Variáveis de ambiente (process.env):", process.env);
