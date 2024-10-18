e665fca06264f7b06831015c422473f864c26574
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2024-10-17 22:41:32 -0300

    feat: day #026 complete, add js files

---
+++ day026/day026.js
@@ -0,0 +1,12 @@
+function add(a, b) {
+    return a + b;
+}
+
+function main() {
+    const result = add(2, 3);
+    console.log(`Resultado: ${result}`);
+    debugger;
+}
+
+main();
+console.log('Executando day026.js');
